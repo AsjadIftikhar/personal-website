6370c6aa62164f5e8483854dbbff48f9b7676463
Asjad <iftikharasjad@gmail.com>
2025-01-16 16:27:56 +0500

    updated meta tags

--- components/Head.jsx
+++ components/Head.jsx
@@ -6,19 +6,19 @@ const CustomHead = ({ title }) => {
       <title>{title}</title>
       <meta
         name="description"
-        content="Nitin Ranganath is an avid full stack web developer building websites and applications you'd love to use"
+        content="Asjad is an avid full stack web developer building websites and applications you'd love to use"
       />
       <meta
         name="keywords"
-        content="nitin ranganath, nitin, ranganath, web developer portfolio, nitin web developer, nitin developer, mern stack, nitin ranganath portfolio, vscode-portfolio"
+        content="web developer, django, react, generative AI portfolio, AWS"
       />
-      <meta property="og:title" content="Nitin Ranganath's Portfolio" />
+      <meta property="og:title" content="Asjad Iftikhars's Portfolio" />
       <meta
         property="og:description"
         content="A full-stack developer building websites that you'd like to use."
       />
-      <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
-      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
+      <meta property="og:image" content="https://drive.google.com/file/d/1_89AIol_yn0Jl4XFC4O56u-EyNqlJaAD/view?usp=sharing" />
+      <meta property="og:url" content="https://asjadiftikhar.site/" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
