dd07d905ece51df8be6f9157409ecf6d9dd64f5a
Asjad <iftikharasjad@gmail.com>
2025-01-19 13:28:01 +0500

    use simple img tag

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,10 +1,9 @@
-import Image from 'next/image';
 import styles from '../styles/ProjectCard.module.css';
 
 const ProjectCard = ({project}) => {
     return (
         <div className={styles.card}>
-            <Image src={project.image} height={project.height || 300} width={project.width || 550} alt={project.name}/>
+            <img src={project.image} height={project.height || 300} width={project.width || 550} alt={project.name}/>
             <div className={styles.content}>
                 <h3>{project.name}</h3>
                 <p>{project.description}</p>
